32e760ae76c6d070ea7b530cd05079e545b4d9ff
Vladimir <intfloatbool@gmail.com>
2019-11-04 15:51:49 +0300

    Winner highlight

---
+++ Assets/Scripts/GameProcess/WinnerHighligther.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class WinnerHighligther : OnGameEndTrigger
+{
+    private float _speed = 5f;
+    private Vector3 _neededLocalPos = new Vector3(5, 10, 0);
+    private Vector3 _neededLocalEulers = new Vector3(60, -90, 0);
+    [SerializeField] private ImageResizer _imgResizer;
+    [SerializeField] private Camera _mainCam;
+
+    private bool _isActivated;
+    
+    private Transform CamTransform => _mainCam.transform;
+    protected override void OnGameEnd(RobotCommandRunner winner)
+    {
+        Robot winnerRobot = winner.Robot;
+        CamTransform.parent = winnerRobot.transform;
+        _imgResizer.IsActive = true;
+        _isActivated = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_isActivated)
+            return;
+        CamTransform.localPosition =
+            Vector3.Lerp(CamTransform.localPosition, _neededLocalPos, _speed * Time.fixedDeltaTime);
+        CamTransform.localRotation = Quaternion.Lerp(CamTransform.localRotation, Quaternion.Euler(_neededLocalEulers), _speed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/UI/ImageResizer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageResizer : MonoBehaviour
+{
+    [SerializeField] private Vector3 _neededScale = Vector3.one;
+    [SerializeField] private Vector3 _basicScale = Vector3.zero;
+    [SerializeField] private float _speed = 6f;
+    public bool IsActive { get; set; }
+    private Image _img;
+    
+    private void Start()
+    {
+        _img = GetComponent<Image>();
+        transform.localScale = _basicScale;
+    }
+
+    private void Update()
+    {
+        if (!IsActive)
+            return;
+        transform.localScale = Vector3.Lerp(transform.localScale, _neededScale, _speed * Time.deltaTime);
+    }
+    
+}

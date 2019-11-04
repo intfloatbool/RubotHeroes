e2c20d6c2a2981d698e45526f73798c3997beb5f
Vladimir <intfloatbool@gmail.com>
2019-11-04 18:09:24 +0300

    Main menu scene and scene switcher

---
+++ Assets/Scripts/Menu/ScenesSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesSwitcher : MonoBehaviour
+{
+    public void GoToBattleScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoToMenuScene()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void GoToEditCommandsScene()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+}
